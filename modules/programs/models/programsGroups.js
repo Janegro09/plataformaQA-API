2109abbd4fe3213c8529d2b650f90d8ea83de1de
Ramiro Macciuci <ramimacciuci@gmail.com>
2020-06-16 20:33:22 -0300

    models of programs module

---
+++ modules/programs/models/programsGroups.js
@@ -0,0 +1,27 @@
+/**
+ * @fileoverview Modulo Programs
+ * 
+ * @version 1.0
+ * 
+ * @author Soluciones Digitales - Telecom Argentina S.A.
+ * @author Ramiro Macciuci <rmacciucivicente@teco.com.ar>
+ * @copyright Soluciones Digitales - Telecom Argentina
+ * 
+ * History:
+ * 1.0 - Version principal
+ */
+
+const includes = require('../../includes');
+
+// Schemas
+const helper = require('../helper');
+const Schemas = {
+    programs: require('../migrations/programs.table')
+}
+
+class ProgramsGroups {
+
+}
+
+
+module.exports = ProgramsGroups;
