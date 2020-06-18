d55bc87409977210d85eb5011727b9c31df81c6f
Ramiro Macciuci <ramimacciuci@gmail.com>
2020-06-18 18:27:06 -0300

    ssl

--- index.js
+++ index.js
@@ -33,66 +33,18 @@ mongoose.connect(`mongodb://${dbConfig.mongodb.user}:${dbConfig.mongodb.password
     console.log("-------------------------------------------------- ");
     console.log("MAIN SERVER CONNECTION");
     console.log("");
-        if(process.env.ENVRIORMENT != 'development'){
-            const https = require('https');
-            const fs = require('fs');
-        
-            var key = fs.readFileSync('/etc/ssl/wildcard.solucionesdigitalesteco.com.ar.key');
-            var cert = fs.readFileSync('/etc/ssl/_.solucionesdigitalesteco.com.ar.cer');
-            var options = {
-                key: key,
-                cert: cert
-            };
-        
-            var server = https.createServer(options, app);
-        
-            server.listen(port, () => {
-                console.log(`Servidor HTTPS corriendo correctamente en puerto: ${port}`);
-                console.log("-------------------------------------------------- ");
-                console.log("-------------------------------------------------- ");
-                console.log("PROJECT INFO");
-                console.log("");
-                console.log(`Comapany Name: ${cfile.projectInformation.company}`);
-                console.log(`Authors: `);
-                cfile.projectInformation.author.map((value) => {
-                    console.log("-> ",value);
-                })
-                console.log("-------------------------------------------------- ");
-            });
-        }else {
-            app.listen(port, () => {
-                console.log(`Servidor HTTP corriendo correctamente en puerto: ${port}`);
-                console.log("-------------------------------------------------- ");
-                console.log("-------------------------------------------------- ");
-                console.log("PROJECT INFO");
-                console.log("");
-                console.log(`Comapany Name: ${cfile.projectInformation.company}`);
-                console.log(`Authors: `);
-                cfile.projectInformation.author.map((value) => {
-                    console.log("-> ",value);
-                })
-                console.log("-------------------------------------------------- ");
+        app.listen(port, () => {
+            console.log(`Servidor HTTP corriendo correctamente en puerto: ${port}`);
+            console.log("-------------------------------------------------- ");
+            console.log("-------------------------------------------------- ");
+            console.log("PROJECT INFO");
+            console.log("");
+            console.log(`Comapany Name: ${cfile.projectInformation.company}`);
+            console.log(`Authors: `);
+            cfile.projectInformation.author.map((value) => {
+                console.log("-> ",value);
             })
-        }
-
+            console.log("-------------------------------------------------- ");
+        })
 }).catch(err => console.log(err));
-
-if(process.env.ENVRIORMENT != 'development'){
-    const https = require('https');
-    const fs = require('fs');
-    const puertoSSL = 8080;
-
-    var key = fs.readFileSync('/etc/ssl/wildcard.solucionesdigitalesteco.com.ar.key');
-    var cert = fs.readFileSync('/etc/ssl/_.solucionesdigitalesteco.com.ar.cer');
-    var options = {
-        key: key,
-        cert: cert
-    };
-
-    var server = https.createServer(options, app);
-
-    server.listen(puertoSSL, () => {
-        console.log("server HTTPS on port : " + port)
-    });
-}
 
