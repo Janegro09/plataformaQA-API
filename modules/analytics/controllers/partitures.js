3cafb23bd6738cc41feff8d5f4df7c4ef0b48596
Ramiro Macciuci <ramimacciuci@gmail.com>
2020-07-01 01:21:36 -0300

    routes for partitures and partitures models

---
+++ modules/analytics/controllers/partitures.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview Modulo Analytics | Partituras
+ * 
+ * @version 1.0
+ * 
+ * @author Soluciones Digitales - Telecom Argentina S.A.
+ * @author Ramiro Macciuci <rmacciucivicente@teco.com.ar>
+ * @copyright Soluciones Digitales - Telecom Argentina
+ * 
+ * History:
+ * 1.0 - Version principal
+ */
+const includes = require('../../includes');
+
+const controller = {
+    async new(req, res){
+
+    },
+    async get(req, res){
+
+    },
+    async delete(req, res) {
+
+    },
+    async update(req, res){
+        
+    }
+}
+
+module.exports = controller
---
+++ modules/analytics/controllers/partituresModels.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview Modulo Analytics | Modelos de partituras - repositorio de partituras
+ * 
+ * @version 1.0
+ * 
+ * @author Soluciones Digitales - Telecom Argentina S.A.
+ * @author Ramiro Macciuci <rmacciucivicente@teco.com.ar>
+ * @copyright Soluciones Digitales - Telecom Argentina
+ * 
+ * History:
+ * 1.0 - Version principal
+ */
+const includes = require('../../includes');
+
+
+const controller = {
+    async new(req, res){
+
+    },
+    async get(req, res){
+
+    },
+    async delete(req, res) {
+
+    },
+    async update(req, res){
+        
+    }
+}
+
+module.exports = controller
