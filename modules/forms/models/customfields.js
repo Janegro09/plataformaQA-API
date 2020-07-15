ec9524d28fbed3b9cb75a72a75a7cb09848abbdc
Ramiro Macciuci <ramimacciuci@gmail.com>
2020-07-15 11:09:16 -0300

    new customfields method

--- modules/forms/models/customfields.js
+++ modules/forms/models/customfields.js
@@ -15,6 +15,7 @@ const includes = require('../../includes');
 
 // Schemas
 const helper = require('../helper');
+const customFieldsSchema = require('../migrations/customfields.table');
 
 module.exports = class customFields {
     constructor(dataobject) {
@@ -41,18 +42,39 @@ module.exports = class customFields {
 
         if(this.type === 'radio' || this.type === 'checkbox' || this.type === 'select'){
             if(!this.values || typeof this.values !== 'object') throw new Error('El campo values es requerido para el tipo de campo ingresado y debe ser un array')
+        }else {
+            this.values = false;
         }
 
         const authorizedValuesofSection = ['M', 'P']
         if(this.section){
             if(!authorizedValuesofSection.includes(this.section)) throw new Error('Debe ingresar solo los nombres de secciones autorizados');
         } else throw new Error('El parametro section es requerido')
+
+        let dataReturn = {
+            required: this.required
+        };
+        for(let i in this){
+            if(i === 'required') continue;
+            if(this[i] !== false){
+                dataReturn[i] = this[i]
+            }
+        }
+
+        return dataReturn;
     }
 
     async save() {
-        this.validarValores();
+        const data = this.validarValores();
 
-        return true;
+        let d = new customFieldsSchema(data)
+        try {
+            let c = await d.save()
+            if(c) return true;
+            else return false;
+        } catch(e) {
+            throw e
+        }
     }
 
     async update() {
