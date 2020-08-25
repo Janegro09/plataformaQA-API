643a16791fb44ea99238a8f8154f95a0bf01d210
Ramiro Macciuci <ramimacciuci@gmail.com>
2020-08-25 10:18:05 -0300

    iniciando modulo qa

---
+++ modules/qa/routes.js
@@ -0,0 +1,22 @@
+/**
+ * @fileoverview Modulo Programs
+ * 
+ * @version 1.0
+ * 
+ * @author Soluciones Digitales - Telecom Argentina S.A.
+ * @author Ramiro Macciuci <rmacciucivicente@teco.com.ar>
+ * @copyright Soluciones Digitales - Telecom Argentina
+ * 
+ * History:
+ * 1.0 - Version principal
+ */
+
+// Incluimos controladores, modelos, schemas y modulos
+const includes      = require('../includes');
+let router          = includes.express.Router();
+
+// Controladores
+
+// router.get('/')
+
+module.exports = router;
