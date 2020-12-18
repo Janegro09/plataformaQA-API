70bf29b61f9c4a7be40201d17c8b32029dd7793b
Ramiro Macciuci <ramimacciuci@gmail.com>
2020-12-18 15:11:25 -0300

    s

--- models/groups.js
+++ models/groups.js
@@ -171,9 +171,11 @@ class Groups {
         let ReturnData = [], tempData,c;
         for(let x = 0; x < consulta.length; x++){
             c = await groupsSchema.find({_id: consulta[x].groupId});
-            tempData = {
-                id: consulta[x].groupId,
-                name: c[0].group
+            if(c.length > 0) {
+                tempData = {
+                    id: consulta[x].groupId,
+                    name: c[0].group
+                }
             }
             ReturnData.push(tempData);
         }
