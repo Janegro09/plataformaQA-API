3e6fb6de04cff100431a9bc738ff4df17076b513
Ramiro Macciuci <ramimacciuci@gmail.com>
2020-12-18 15:14:38 -0300

    s

--- app.js
+++ app.js
@@ -43,18 +43,19 @@ try {
 if(process.env.ENVRIORMENT == 'development'){
     app.use(cors()) // Permitimos todos los request sin importar providencia
 }else {
-    const whiteListURLS = cfile.whiteListCors; // Lista de URLs permitidas en producción, especificadas en el archivo de configuracion.
+    // const whiteListURLS = cfile.whiteListCors; // Lista de URLs permitidas en producción, especificadas en el archivo de configuracion.
 
-    let crs = function (req, callback) {
-        var corsOptions;
-        if (whiteListURLS.indexOf(req.header('Origin')) !== -1) {
-          corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-        } else {
-          corsOptions = { origin: false } // disable CORS for this request
-        }
-        callback(null, corsOptions) // callback expects two parameters: error and options
-    }
-    app.use(cors(crs))
+    // let crs = function (req, callback) {
+    //     var corsOptions;
+    //     // if (whiteListURLS.indexOf(req.header('Origin')) !== -1) {
+    //     if (whiteListURLS.indexOf(req.header('Origin')) !== -1) {
+    //       corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    //     } else {
+    //       corsOptions = { origin: false } // disable CORS for this request
+    //     }
+    //     callback(null, corsOptions) // callback expects two parameters: error and options
+    // }
+    app.use(cors())
 }
 
 app.use(fileUpload()); // Permito la posibilidad de enviar archivos por formularios, si la desactivo, los formularios dejaran de ser form-data y sera xxx-form-data/urlencoded
